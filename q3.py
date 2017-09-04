45f724e89dcb7808555e723db266d6ff1ec20ecf
soham1705 <soham.m15@iiits.in>
2017-09-04 22:00:45 +0530

    Finish q3 median filter (complete q3)

--- q3.py
+++ q3.py
@@ -16,11 +16,30 @@ def GaussianFilter(img):
 
 	return transformed_img.astype(int)
 
+
+def MedianFilter(img):
+
+	N,M=img.shape
+	img=np.pad(img,(1,),'constant',constant_values=0)
+	transformed_img=np.zeros([N,M])
+
+	for row in range(N):
+		for col in range(M):
+			transformed_img[row][col]=np.median(img[row:row+3,col:col+3])
+
+	return transformed_img
+
+
 if __name__=='__main__':
 
 	img=cv2.imread('q3_input.jpg')
 	img=cv2.split(img)[0]
 
-	img=GaussianFilter(img)
+	final1=GaussianFilter(img)
 
-	cv2.imwrite('q3_output_gaussian.jpg',img)
+	cv2.imwrite('q3_output_gaussian.jpg',final1)
+
+	final2=MedianFilter(img)
+
+	cv2.imwrite('q3_output_median.jpg',final2)
+
